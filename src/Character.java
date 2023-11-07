10958735cfc6afea92a7e475d4a333ea1fd096dd
recruta23 <144141636+recruta23@users.noreply.github.com>
2023-11-07 11:16:52 -0300

    Criação  da classe Character.java
    Classe que engloba detalhes dos personagens

---
+++ src/Character.java
@@ -0,0 +1,16 @@
+public class Character {
+    private String nome;
+    private String especie;
+    private String status;
+    private String[] episodios;
+    private String resumo;
+
+    public Character(String nome, String especie, String status, String[] episodios, String resumo) {
+        this.nome = nome;
+        this.especie = especie;
+        this.status = status;
+        this.episodios = episodios;
+        this.resumo = resumo;
+    }
+
+}
