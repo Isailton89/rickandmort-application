2b60046e3f1e1c3138364b5d62578419d21eb1eb
recruta23 <144141636+recruta23@users.noreply.github.com>
2023-11-07 11:19:44 -0300

    Atualização do nome da classe
    Alteração do nome da classe Character.java para Personagens.java

--- src/Personagens.java
+++ src/Personagens.java
@@ -1,11 +1,11 @@
-public class Character {
+public class Personagens {
     private String nome;
     private String especie;
     private String status;
     private String[] episodios;
     private String resumo;
 
-    public Character(String nome, String especie, String status, String[] episodios, String resumo) {
+    public Personagens(String nome, String especie, String status, String[] episodios, String resumo) {
         this.nome = nome;
         this.especie = especie;
         this.status = status;
